f4005350884105461db64801eb097848a613e9c9
bonkooo <143710575+bonkooo@users.noreply.github.com>
2024-07-12 19:48:43 +0200

    Update huffman.cpp

--- huffman.cpp
+++ huffman.cpp
@@ -15,8 +15,8 @@ Node::Node(char c, int frq, Node* left, Node* right) {
 
 //struktura sa poredbenom funkcijom za prioritetni red
 struct cmp {
-    bool op(Node* left, Node* right) {
-        if (left->frq > right->frq) {
+    bool operator()(Node* first, Node* second) {
+        if (first->frq > second->frq) {
             return true;
         }
         return false;
@@ -31,19 +31,61 @@ Node* createTree(const unordered_map<char, int>& frqMap)
     for (auto pair : frqMap) {
         pq.push(new Node(pair.first, pair.second));
     }
+
+    //formiraj stablo
+    while (pq.size() != 1) {
+        Node *left = pq.top();
+        pq.pop();
+        Node* right = pq.top();
+        pq.pop();
+
+        int newFrq = left->frq + right->frq; 
+        pq.push(new Node('\0', newFrq, left, right)); //ubaci novo podstablo/cvor u red
+    }
+
+    Node* root = pq.top();
+    return root;
 }
 
-void generateCodes(Node* root, string& str, unordered_map<char, string>& codeMap)
-{
+void generateCodes(Node* root, const string& str, unordered_map<char, string>& codeMap)
+{   
+    if (!root) return; //terminalni uslov za rekurziju
 
+    if (!root->left && !root->right) { //karakteri su u listovima stabla
+        codeMap[root->c] = str; //updateuj kod za karakter
+    }
+
+    generateCodes(root->left, str + "0", codeMap);
+    generateCodes(root->left, str + "1", codeMap);
 }
 
 string encode(string& message, unordered_map<char, string>& codeMap)
 {
-    return string();
+    stringstream ss;
+    for (char ch : message) {
+        ss << codeMap[ch];
+    }
+    return ss.str();
 }
 
 string decode(string& code, Node* root)
 {
-    return string();
+    stringstream ss;
+    Node* curr = root;
+    for (char bit : code) {
+        if (bit == '0') {
+            curr = curr->left;
+        }
+        else if (bit == '1') {
+            curr = curr->right;
+        }
+
+        //ako smo naisli na list (karakter) upisemo ga
+        if (!curr->left && !curr->right) {
+            ss << curr->c;
+            curr = root; //vratimo se u koren i trazimo novi karakter
+        }
+    }
+    
+    return ss.str();
 }
